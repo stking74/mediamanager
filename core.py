cf363f337d2dad3be670be677e63bb072143c1fa
Tyler King <stking74@gmail.com>
2025-02-13 10:17:43 -0500

    File and FileTree utility methods
    Created methods for adding tags both to individual Files and to FileTree contents, with recursive option. Added docstrings for some methods. Minor formatting edits and bugfixes.

--- core.py
+++ core.py
@@ -6,9 +6,7 @@ Created on Sun May  1 09:54:21 2022
 @author: tyler
 """
 
-import os
-import hashlib
-import json
+import os, hashlib, json, shutil
 
 class File:
 
@@ -26,7 +24,7 @@ class File:
         self.tags = []
 
         if not os.path.isabs(filename):
-            filename = os.path.asabs(filename)
+            filename = os.path.abspath(filename)
 
         self.long_name = filename
         self.location, self.short_name = os.path.split(filename)
@@ -35,6 +33,13 @@ class File:
             self.extension = self.short_name.split('.')[-1]
         except IndexError:
             print(f'Could not identify file extension for file {filename}')
+            
+        self._scan_params_(gethash=gethash)
+        
+        return
+    
+    def _scan_params_(self, gethash=False):
+        
         try:
             self.size = os.path.getsize(self.long_name)
         except:
@@ -51,6 +56,11 @@ class File:
             print(f'File {self.long_name} already tagged "{tag}".')
         else:
             self.tags.append(tag)
+        return
+    
+    def add_tags(self, tag_list):
+        for tag in tag_list:
+            self.add_tag(tag)
         return
     
     def remove_tag(self, tag):
@@ -142,6 +152,53 @@ class File:
             print(f'WARNING:File {self.fullname} could not be deleted!')
         return
     
+    def rescan(self, gethash=False):
+        '''
+        Wrapper for self._scan_params_
+        Intended use is to update file parameters after possible modification.
+
+        Parameters
+        ----------
+        gethash : bool, optional
+            Sets whether hash should be recalculated. The default is False.
+
+        Returns
+        -------
+        None.
+
+        '''
+        self._scan_params_(gethash)
+        return
+    
+    def move(self, new_path):
+        '''
+        Moves data associated with File to a new location on the hard drive,
+        updates relevant attributes. Does not update file position in any
+        linked FileTrees.
+
+        Parameters
+        ----------
+        new_path : str
+            Absolute path of directory to which the file will be moved.
+
+        Returns
+        -------
+        None.
+
+        '''
+        #Create helper variables
+        src = self.long_name
+        dst = os.path.join(new_path, self.short_name)
+        #Move file data
+        shutil.move(src, dst)
+        #Update File attributes
+        self.long_name = dst
+        head, tail = os.path.split(self.long_name)
+        self.short_name = tail
+        self.location = new_path
+        return
+        
+    
 class FileTree(dict):
     
     def __init__(self):
@@ -178,14 +235,17 @@ class FileTree(dict):
     def from_dict(decomposed):
         
         filetree = FileTree()
-        filetree.root = list(decomposed.keys())[0]
+        # if len(decomposed) == 0:
+        #     return filetree
+        # filetree.root = list(decomposed.keys())[0]
         for key, (item_type, item) in decomposed.items():
             if item_type == 'file':
                 file = File.fromdict(item)
-                filetree[file.short_name] = File
+                filetree[file.short_name] = file
                 filetree.size += file.size
             elif item_type == 'dir':
                 subdir = FileTree.from_dict(item)
+                filetree.root = key
                 filetree[os.path.split(key)[1]] = subdir
                 filetree.size += subdir.size
                 
@@ -231,6 +291,24 @@ class FileTree(dict):
         return filetree[list(filetree.keys())[0]]
 
     def find_duplicates(self, filters=None):
+        '''
+        Searches for duplicate files within filetree. Duplicates are detected
+        by comparing SHA-256 hashes. Calculates hash for all files if not
+        calculated previously.
+        
+        Filters parameter currently not functional.
+
+        Parameters
+        ----------
+        filters : TYPE, optional
+            DESCRIPTION. The default is None.
+
+        Returns
+        -------
+        duplicates : dict
+            Dictionary summarizing all detected duplicate files.
+
+        '''
         flattened = self.flatten()
         hashes = {}
 
@@ -261,6 +339,37 @@ class FileTree(dict):
                 for path, item in subdir_flat.items():
                     flattened[path] = item
         return flattened
+    
+    def add_tag(self, tag, recursive=False):
+        '''
+        Add specified tag to all files in top-level directory of filetree.
+        Set recursive=True to also apply tag to all subdirectories.
+
+        Parameters
+        ----------
+        tag : str
+            Tag to be added to all files.
+        recursive : bool, optional
+            Specifies whether to add tag to all subdirectories, as well as
+            top-level directory. The default is False.
+
+        Returns
+        -------
+        None.
+
+        '''
+        for item in self.values():
+            if type(item) == File:
+                item.add_tag(tag)
+            elif type(item) == FileTree:
+                if recursive == True:
+                    item.add_tag(tag)
+        return
+    
+    def add_file(self, filename):
+        newfile = File(filename)
+        self[newfile.short_name] = newfile
+        return
 
 class Directory(dict):
 
